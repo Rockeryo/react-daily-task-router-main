234a4c958361cfdbbf64ed4cb41acd2bf83a960d
rock <mohist_15@hotmail.com>
2022-08-21 12:16:00 +0800

    動態路由

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Routes,
   Route,
   useNavigate,
+  useParams,
+  Outlet
 } from 'react-router-dom';
 
 const Logout= () => {
@@ -21,7 +23,16 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
-
+const Post = () => {
+  return <div>
+    <h3>詳細資料</h3>
+    <Outlet />
+  </div>
+}
+const ProfilePage = () => {
+  let params = useParams();
+  return <p>PostID: {params.postId}</p>;
+}
 function App() {
   return (
     <div className="container">
@@ -39,12 +50,21 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<ProfilePage />} />
+          </Route>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
